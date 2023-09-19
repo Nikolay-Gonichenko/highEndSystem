c234e040a96a0909fa25070a3b0e5c50f194753b
Gonichenko Nikolay <nick-kg@yandex.ru>
2023-09-19 22:34:43 +0300

    Initial commit

---
+++ src/main/java/ru/itmo/highendsystem/HighEndSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.highendsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighEndSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighEndSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itmo/highendsystem/HighEndSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.highendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighEndSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
